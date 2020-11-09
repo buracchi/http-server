52a8e425ab97cd69131a818cdf938f24f52b9dd4
Fabio Buracchi <45599613+buracchi@users.noreply.github.com>
2020-11-09 23:38:10 +0100

    Update dependencies management

--- build.sh
+++ build.sh
@@ -4,13 +4,7 @@
 # This file is just an utility and it is not necessary to the project.
 #
 
-git clone https://github.com/microsoft/vcpkg .vcpkg
-./.vcpkg/bootstrap-vcpkg.sh
-./.vcpkg/vcpkg install libjpeg-turbo
-
-mkdir -p build
-cd build
-:
-cmake -DCODE_COVERAGE=ON -DCMAKE_BUILD_TYPE=Debug ..
-cmake --build . --config Debug -j $(nproc)
-ctest -j $(nproc) --output-on-failure
+./vcpkg/bootstrap-vcpkg.sh
+mkdir build
+cmake -S . -B build
+cmake --build build -j $(nproc)
